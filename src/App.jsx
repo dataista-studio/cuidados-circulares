1a582cd55848429f9d7a13d3213514c1fb1b8174
katynewlin <51867820+katynewlin@users.noreply.github.com>
2026-04-06 19:07:46 -0300

    add Portada component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Portada from './components/Portada.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,23 +11,7 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        <Portada />
       </section>
 
       <div className="ticks"></div>
---
+++ src/components/Portada.jsx
@@ -0,0 +1,7 @@
+export default function Portada() {
+    return (
+        <div>
+            Portada
+        </div>
+    )
+};
