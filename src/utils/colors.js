efc28f4e6cce99dbcbc8bd867a74e9028091aea4
katynewlin <51867820+katynewlin@users.noreply.github.com>
2026-04-08 08:56:37 -0300

    add js colors

---
+++ src/utils/colors.js
@@ -0,0 +1,22 @@
+export const verdeOscuro = "#72744A";
+export const verdeMedio = "#7C6A0A";
+export const verde = "#959848";
+export const verdeClaro = "#BBBD8D";
+export const salmon = "#FFDEC6";
+export const naranjoClaro = "#FA9500";
+export const naranjo = "#EB6424";
+export const naranjoOscuro = "#CC4201";
+
+/*  Neutros */
+export const crema = "#F7EEE1";
+export const burdeo = "#5D202E";
+export const blanco = "#FFFFFF";
+export const gris = "#E8E5E1";
+
+/* Globe */
+export const oceano3 = "#FFDAC6";
+export const tierra3 = "#E5AE82";
+export const pais3 = "#652030"
+export const oceano5 = "#ECD9BE";
+export const tierra5 = "#D4AE7C";
+export const pais5 = "#5D202E"
