f147c04fb7f7194a946c212e1f6268eb89b9a38c
katynewlin <51867820+katynewlin@users.noreply.github.com>
2026-04-08 19:55:04 -0300

    remove unused logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Portada from './screens/Portada.jsx';
 import SeccionI from './screens/SeccionI.jsx'
@@ -47,4 +44,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
