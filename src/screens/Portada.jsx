b6b1f1984596ddeaa7a8869826ee1f6c451a5021
Svillalon <svillalon0@gmail.com>
2026-04-08 16:54:22 -0400

    Create first section page

--- src/screens/Portada.jsx
+++ src/screens/Portada.jsx
@@ -1,11 +1,29 @@
 import ButtonArrow from "../ui/ButtonArrow"
 
+
+
 export default function Portada() {
     return (
-        <div>
-            Portada
-            <ButtonArrow direction='left' />
-            <ButtonArrow direction='right' />
+        <div className="w-screen h-screen bg-verde-medio flex items-center justify-center">
+    
+        <div className="flex flex-col items-center justify-center gap-24 text-center">
+            
+            <p className="text-[216px] text-salmon">Cuidados</p>
+
+            <img 
+            src="./src/assets/img/hands.png" 
+            alt="" 
+            className="h-[227px] w-[223px]" 
+            />
+
+            <p className="text-[216px] text-salmon">Circulares</p>
+
+            <p className="text-verde-claro text-[32px] text-center max-w-[900px]">
+                Mujeres que cuidan mientras necesitan ser cuidadas en América Latina y el Caribe.
+            </p>
+
         </div>
+
+    </div>
     )
 };
