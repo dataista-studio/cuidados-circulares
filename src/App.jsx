742d501f84f87b21b928a6f4b8b50aa4faf386e9
Svillalon <svillalon0@gmail.com>
2026-04-08 13:42:38 -0400

    Create section files and folders

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Portada from './screens/Portada.jsx';
+import SeccionI from './screens/SeccionI.jsx'
+import SeccionII from './screens/SeccionIII.jsx'
+import SeccionIII from './screens/SeccionIII.jsx'
+import SeccionIV from './screens/SeccionIV.jsx'
 import SeccionV from './screens/SeccionV.jsx'
+import Cierre from './screens/Cierre.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,9 +20,29 @@ function App() {
         <Portada />
       </section>
 
+      <section id="que-son-los-cuidados">
+        <SeccionI/>
+      </section>
+
+      <section id="el-tiempo-no-se-reparte-igual">
+        <SeccionII/>
+      </section>
+
+      <section id="detras-del-tiempo-las-tareas">
+        <SeccionIII/>
+      </section>
+
       <section id="espectro-demografico">
+        <SeccionIV />
+      </section>
+
+      <section id="personas-receptoras-del-cuidado">
         <SeccionV />
       </section>
+
+      <section id="cierre">
+        <Cierre />
+      </section>      
     </>
   )
 }
