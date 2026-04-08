fc5ee36f43e76457acee00a01ce6b63d84ac5e9e
katynewlin <51867820+katynewlin@users.noreply.github.com>
2026-04-08 19:59:08 -0300

    switch from p to h1 and remove unnecessary div

--- src/screens/Portada.jsx
+++ src/screens/Portada.jsx
@@ -1,25 +1,19 @@
 export default function Portada() {
     return (
-        <div className="w-screen h-screen bg-verde-medio flex items-center justify-center">
-        
-            <div className="flex flex-col items-center justify-center gap-24 text-center">
-                
-                <p className="text-[216px] text-salmon">Cuidados</p>
+        <div className="w-screen h-screen bg-verde-medio flex flex-col gap-24 text-center items-center justify-center">
+            <h1 className="text-[216px] text-salmon">Cuidados</h1>
 
-                <img 
-                    src="./src/assets/img/hands.png" 
-                    alt="" 
-                    className="h-[227px] w-[223px]" 
-                />
+            <img 
+                src="./src/assets/img/hands.png" 
+                alt="" 
+                className="h-[227px] w-[223px]" 
+            />
 
-                <p className="text-[216px] text-salmon">Circulares</p>
+            <h1 className="text-[216px] text-salmon">Circulares</h1>
 
-                <p className="text-verde-claro text-[32px] text-center max-w-[900px]">
-                    Mujeres que cuidan mientras necesitan ser cuidadas en América Latina y el Caribe.
-                </p>
-
-            </div>
-
+            <p className="text-verde-claro text-[32px] text-center max-w-[900px]">
+                Mujeres que cuidan mientras necesitan ser cuidadas en América Latina y el Caribe.
+            </p>
         </div>
     )
 };
