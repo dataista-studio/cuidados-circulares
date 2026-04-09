46d472d4b063b62ab6d34276ab06698fefdd4683
Svillalon <svillalon0@gmail.com>
2026-04-09 13:54:12 -0400

    Adding animation to other hands

--- src/screens/SeccionI.jsx
+++ src/screens/SeccionI.jsx
@@ -27,12 +27,32 @@ export default function SeccionI() {
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
-    const movement =
-        progress < 0.5
-            ? progress * 2
-            : (1 - progress) * 2;
 
-    const translateX = -100 + movement * 100;
+    const getTransform = ({ direction = "left", distance = 100 }) => {
+        const movement =
+            progress < 0.5
+                ? progress * 2
+                : (1 - progress) * 2;
+    
+        const value = -distance + movement * distance;
+    
+        switch (direction) {
+            case "left":   // entra desde izquierda
+                return `translateX(${value}%)`;
+    
+            case "right":  // entra desde derecha
+                return `translateX(${-value}%)`;
+    
+            case "bottom": // entra desde abajo
+                return `translateY(${-value}%)`;
+    
+            case "top":    // entra desde arriba
+                return `translateY(${value}%)`;
+    
+            default:
+                return "";
+        }
+    };
 
     return (
         <div ref={sectionRef} className="w-screen h-[100vh] bg-verde-claro relative overflow-hidden">
@@ -46,17 +66,49 @@ export default function SeccionI() {
                 alt=""
                 className="h-[237.3px] w-[619.71px] absolute left-0 top-1/2 -translate-y-1/2"
                 style={{
-                    transform: `translateX(${translateX}%) translateY(-50%)`
+                    transform: `${getTransform({ direction: "left" })} translateY(-50%)`
                 }}
             />
 
-            <img src="./src/assets/img/big_hand_r.png" alt="" className="h-[237.3px] w-[619.71px] absolute right-0" />
-            
-            <img src="./src/assets/img/medium_hand_l.png" alt="" className="w-[221.41px] h-[490.74px] absolute bottom-0 right-1/2" />
-            <img src="./src/assets/img/medium_hand_r.png" alt="" className="w-[221.41px] h-[490.74px] absolute bottom-0 left-1/2" />
+            <img
+                src="./src/assets/img/big_hand_r.png"
+                alt=""
+                className="h-[237.3px] w-[619.71px] absolute right-0 top-1/2 -translate-y-1/2"
+                style={{
+                    transform: `${getTransform({ direction: "right" })} translateY(-50%)`
+                }}
+            />
 
-            <img src="./src/assets/img/small_hand_l.png" alt="" className="w-[82px] h-[277px] absolute bottom-0 right-1/2" />
-            <img src="./src/assets/img/small_hand_r.png" alt="" className="w-[82px] h-[277px] absolute bottom-0 left-1/2" />
+            <img
+                src="./src/assets/img/medium_hand_l.png"
+                alt=""
+                className="w-[221.41px] h-[490.74px] absolute bottom-0 right-[60%]"
+                style={{
+                    transform: `${getTransform({ direction: "bottom", distance: 120 })} translateX(50%)`
+                }}
+            />
+
+            <img
+                src="./src/assets/img/medium_hand_r.png"
+                alt=""
+                className="w-[221.41px] h-[490.74px] absolute bottom-0 right-[30%] "
+                style={{
+                    transform: `${getTransform({ direction: "bottom", distance: 120 })} translateX(-50%)`
+                }}
+            />
+
+            <img src="./src/assets/img/small_hand_l.png" alt="" className="w-[82px] h-[277px] absolute bottom-0 left-[42.5%]" 
+                style={{
+                    transform: `${getTransform({ direction: "bottom", distance: 120 })} translateX(50%)`
+                }}
+            />
+            <img src="./src/assets/img/small_hand_r.png" alt="" className="w-[82px] h-[277px] absolute bottom-0 right-[42.5%]" 
+            
+            style={{
+                transform: `${getTransform({ direction: "bottom", distance: 120 })} translateX(-50%)`
+            }}
+
+            />
 
         </div>
     );
