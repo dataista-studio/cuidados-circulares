993bdfc94c880b51e00372b971eafd63d4d7bab0
Svillalon <svillalon0@gmail.com>
2026-04-09 12:12:08 -0400

    Adding section I

--- src/screens/SeccionI.jsx
+++ src/screens/SeccionI.jsx
@@ -1,7 +1,12 @@
 export default function SeccionI() {
     return (
-        <div className="w-screen h-screen bg-red-500">
-            Sección1
+        <div className="w-screen h-screen bg-verde-claro relative">
+
+            <p className="text-[91px] text-blanco max-w-[336px]">Tipos de cuidado</p>
+
+            <img src="./src/assets/img/big_hand_l.png" alt="" className="h-[237.3px] w-[619.71px] absolute left-0" />
+            <img src="./src/assets/img/big_hand_r.png" alt="" className="h-[237.3px] w-[619.71px] absolute right-0" />
+
         </div>
     )
 };
