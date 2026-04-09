700f31b2e9b688f6c8af985589d05f7e64a18c54
katynewlin <51867820+katynewlin@users.noreply.github.com>
2026-04-08 22:51:26 -0300

    add legend

--- src/screens/SeccionV.jsx
+++ src/screens/SeccionV.jsx
@@ -1,22 +1,46 @@
+import { oceano5, tierra5, pais5, verde, naranjo } from "../utils/colors";
+
 import Title from "./seccionV/Title";
 import Paragraph from "./seccionV/Paragraph";
 import CountryTitle from "./seccionV/CountryTitle";
+import Globe from "../components/Globe";
+import ChartTitle from "./seccionV/ChartTitle";
+import ChartSource from "./seccionV/ChartSource";
+import LegendItem from "./seccionV/LegendItem";
 
 const title = "La brecha de cuidados es crónica";
 const paragraphText = "La brecha de género del perfil feminizado de los cuidadores se repite en todos los segmentos etáreos. Las mujeres cuidan más en todos los ciclos de nuestra vida.";
 const selectedCountry = 'México';
+const englishName = "Mexico";
+const chartTitle = "Cantidad de personas cuidadoras principales por rango etáreo, según sexo.";
+const chartSource = "Fuente: Enasic 2022";
 
 export default function SeccionV() {
     return (
-        <div className="w-screen h-screen bg-crema ">
-            <div className="max-w-[1080px] flex flex-col mx-auto h-screen">
-                <div className="w-[48%] h-screen">
+        <div className="w-screen h-screen bg-crema overflow-hidden">
+            <div className="max-w-[1080px] flex flex-row mx-auto h-screen pt-[43px] gap-10 ">
+                <div className="w-[48%] h-full">
                     <Title title={title} />
                     <Paragraph paragraph={paragraphText} />
                     <CountryTitle countryName={selectedCountry} />
+                    <Globe
+                        width={350}
+                        countryName={englishName}
+                        oceanColor={oceano5}
+                        landColor={tierra5}
+                        countryColor={pais5}
+                    />
                 </div>
                 <div className="w-[48%] h-screen">
-                    <Title title={"La brecha de cuidados es crónica"} />
+                    <div className="h-[95%] w-[140%] bg-blanco pt-[49px]">
+                        <div className="w-[247px] ml-[49px]">
+                            <ChartTitle chartTitle={chartTitle} />
+                            <ChartSource chartSource={chartSource} />
+                            {selectedCountry === 'México' ? <LegendItem fill={"none"} stroke={"#000"} text={"100 personas"} /> : null}
+                            <LegendItem fill={verde} stroke={verde} text={"Mujeres"} />
+                            <LegendItem fill={naranjo} stroke={naranjo} text={"Hombres"} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
