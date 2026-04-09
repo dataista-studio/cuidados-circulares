f7dcc3cbc7c57254960a990777746548376f4d02
katynewlin <51867820+katynewlin@users.noreply.github.com>
2026-04-08 21:59:16 -0300

    add title

--- src/screens/SeccionV.jsx
+++ src/screens/SeccionV.jsx
@@ -1,7 +1,17 @@
+import Title from "./seccionV/Title"
+
 export default function SeccionV() {
     return (
-        <div className="w-screen h-screen bg-red-500">
-            Sección5
+        <div className="w-screen h-screen bg-crema ">
+            <div className="max-w-[1080px] flex flex-col mx-auto h-screen">
+                <div className="w-[48%] h-screen">
+                    <Title title={"La brecha de cuidados es crónica"} />
+                    
+                </div>
+                <div className="w-[48%] h-screen">
+                    <Title title={"La brecha de cuidados es crónica"} />
+                </div>
+            </div>
         </div>
     )
 };
