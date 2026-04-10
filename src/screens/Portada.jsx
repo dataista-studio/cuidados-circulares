21937186cbf3bf10ab58817ab638862b2d55e383
Svillalon <svillalon0@gmail.com>
2026-04-10 19:54:53 -0400

    fix styles

--- src/screens/Portada.jsx
+++ src/screens/Portada.jsx
@@ -6,7 +6,7 @@ export default function Portada() {
             <img 
                 src="./src/assets/img/waves.png" 
                 alt="" 
-                className="h-[237px] w-[827px] object-contain  absolute top-[27%] opacity-0 animate-[fadeUp_1s_ease-out_forwards]" 
+                className="h-[237px] w-[827px] object-contain z-50 absolute top-[17%] opacity-0 animate-[fadeUp_1s_ease-out_forwards]" 
             />
             
             <h1 className="text-[216px] text-salmon font-heading animate-[fadeUp_1s_ease-out_forwards] [animation-delay:0.2s]">Cuidados</h1>
@@ -19,9 +19,15 @@ export default function Portada() {
 
             <h1 className="text-[216px] text-salmon font-heading z-0 opacity-0 animate-[fadeUp_1s_ease-out_forwards] [animation-delay:0.6s]">Circulares</h1>
 
-            <p className="text-crema text-serif text-[32px] text-center max-w-[900px] mt-20 leading-[1.5] opacity-0 animate-[fadeUp_1s_ease-out_forwards] [animation-delay:0.8s]">
-                Mujeres que cuidan mientras necesitan ser cuidadas en América Latina y el Caribe.
+            <p className="text-crema font-serif text-[32px] text-center max-w-[900px] mt-15 leading-[1.5] opacity-0 animate-[fadeUp_1s_ease-out_forwards] [animation-delay:0.8s]">
+                Mujeres que cuidan mientras necesitan ser cuidadas en <br/>América Latina y el Caribe.
             </p>
+
+            <img 
+                src="./src/assets/img/call to action.png" 
+                alt="" 
+                className="h-[100px] w-auto z-10 animate-[fadeUp_1s_ease-out_forwards] [animation-delay:0.4s]" 
+            />
 
             <style>
                 {`
--- src/screens/seccionII/Hands3.jsx
+++ src/screens/seccionII/Hands3.jsx
@@ -88,7 +88,7 @@ export default function Hands3({progress}) {
                         className="absolute inset-0 w-full h-full object-contain"
                     />
 
-                    <p className='relative text-center max-w-[307px] text-verde-oscuro text-[22px]'>
+                    <p className='relative text-center max-w-[307px] text-verde-oscuro font-serif text-[22px]'>
                         Aquellas tareas que implican la interacción de personas, como cambiar de ropa a un bebé, dar de comer a una persona anciana, acompañar a la pareja al centro de salud y hablar por teléfono con un familiar para saber cómo se encuentra.
                     </p>
                 </div>
