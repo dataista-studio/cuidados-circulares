db29a35192e7678ea7366dd039c857ebddea11e4
Svillalon <svillalon0@gmail.com>
2026-08-13 00:02:52 -0400

    Fix styles and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Portada from './screens/Portada.jsx';
-import SeccionI from './screens/SeccionI.jsx'
-import SeccionII from './screens/SeccionII.jsx'
-import SeccionIII from './screens/SeccionIII.jsx'
-import SeccionIV from './screens/SeccionIV.jsx'
-import SeccionV from './screens/SeccionV.jsx'
-import SeccionVI from './screens/SeccionVI.jsx'
-import Cierre from './screens/Cierre.jsx'
-import Credits from './screens/Credits.jsx';
+import { useState } from "react";
+import "./App.css";
+import Portada from "./screens/Portada.jsx";
+import SeccionI from "./screens/SeccionI.jsx";
+import SeccionII from "./screens/SeccionII.jsx";
+import SeccionIII from "./screens/SeccionIII.jsx";
+import SeccionIV from "./screens/SeccionIV.jsx";
+import SeccionV from "./screens/SeccionV.jsx";
+import SeccionVI from "./screens/SeccionVI.jsx";
+import Cierre from "./screens/Cierre.jsx";
+import Credits from "./screens/Credits.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -19,39 +19,41 @@ function App() {
         <Portada />
       </section>
 
-      <section id="que-son-los-cuidados">
-        <SeccionI/>
-      </section>
+      <div className="hidden sm:block">
+        <section id="que-son-los-cuidados">
+          <SeccionI />
+        </section>
 
-      <section id="el-tiempo-no-se-reparte-igual">
-        <SeccionII/>
-      </section>
+        <section id="el-tiempo-no-se-reparte-igual">
+          <SeccionII />
+        </section>
 
-      <section id="detras-del-tiempo-las-tareas">
-        <SeccionIII/>
-      </section>
+        <section id="detras-del-tiempo-las-tareas">
+          <SeccionIII />
+        </section>
 
-      <section id="espectro-demografico">
-        <SeccionIV />
-      </section>
+        <section id="espectro-demografico">
+          <SeccionIV />
+        </section>
 
-      <section id="personas-receptoras-del-cuidado">
-        <SeccionV />
-      </section>
+        <section id="personas-receptoras-del-cuidado">
+          <SeccionV />
+        </section>
 
-      <section id="personas-receptoras-del-cuidado">
-        <SeccionVI />
-      </section>
+        <section id="personas-receptoras-del-cuidado">
+          <SeccionVI />
+        </section>
 
-      <section id="cierre">
-        <Cierre />
-      </section>  
+        <section id="cierre">
+          <Cierre />
+        </section>
 
-      <section id="creditos">
-        <Credits />  
-      </section>    
+        <section id="creditos">
+          <Credits />
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
